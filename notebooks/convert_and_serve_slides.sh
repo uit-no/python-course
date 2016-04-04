fe93fc0720d5f7c0aecaf67be720d4b627f10dfe
Lars Tiede <lars.tiede@gmail.com>
2016-04-04 15:01:42 +0200

    deleted old 'serve slides' script

--- notebooks/convert_and_serve_slides.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# run this in the Vagrant VM to convert and serve a notebook as reveal.js
-# slides on port 8889. Vagrant forwards that port to your host.
-
-jupyter nbconvert "$1" --to slides --post serve --ServePostProcessor.ip=0.0.0.0 --ServePostProcessor.open_in_browser=False --ServePostProcessor.port=8889
