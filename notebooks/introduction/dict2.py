50d15ce5c7be2f3317142692a58e49447f05e23f
Giacomo Tartari <giacomo.tartari@uit.no>
2016-04-05 21:44:40 +0200

    some fixes

--- notebooks/introduction/dict2.py
+++ notebooks/introduction/dict2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-city = {"Vienna":1.741, "London":8.539, "Paris":2.244, "Berlin":3.502, "Zurich":0.378}
+city = {"Vienna": 1.741, "London": 8.539, "Paris": 2.244, "Berlin": 3.502, "Zurich": 0.378}
 
 city["Hamburg"] = 1.734
 
--- notebooks/introduction/present.py
+++ notebooks/introduction/present.py
@@ -4,7 +4,7 @@ text = "Lists and Strings can be accessed via indices!"
 
 lst = ["Vienna", "London", "Paris", "Berlin", "Zurich", "Hamburg"]
 
-city = {"Vienna":1.741, "London":8.539, "Paris":2.244, "Berlin":3.502, "Zurich":0.378}
+city = {"Vienna": 1.741, "London": 8.539, "Paris": 2.244, "Berlin": 3.502, "Zurich": 0.378}
 
 tup = (1,2,3,4)
 
