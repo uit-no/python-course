c00849ba644879b849ce094aadb1733d61ff6af7
Ole Martin Bjørndalen <ole.martin.bjorndalen@uit.no>
2016-04-05 22:04:42 +0200

    Added "Making your own module" slide.

---
+++ notebooks/introduction/simplemath.py
@@ -0,0 +1,8 @@
+ALMOST_PI = 3.14
+
+def add(a, b):
+    return a + b
+
+def mul(a, b):
+    return a * b
+
---
+++ notebooks/introduction/using_simplemath.py
@@ -0,0 +1,6 @@
+import simplemath
+from simplemath import mul, ALMOST_PI
+
+print(simplemath.add(1, 2))
+
+print(mul(2, ALMOST_PI))
