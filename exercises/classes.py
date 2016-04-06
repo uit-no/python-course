8be878f2b861c342377f7b1e10d9c5fb5c555a1c
Lars Tiede <lars.tiede@gmail.com>
2016-04-06 23:07:19 +0200

    updated classes exercises

--- exercises/classes.py
+++ exercises/classes.py
@@ -4,60 +4,67 @@ def make_dog_class():
     * a dog has an attribute `happiness` which is initially set to 100, and
       which is decremented by 1 when time advances.
     * when a dog meets another dog, both dogs' happiness is reset to 100.
-    * a dog is always hungrier than any other pet that is not a dog, regardless
-      of the `hunger` attribute.
-    * when a dog meets a fish, the dog is fed and the fish dies.
+    * when a dog meets a fish, the dog feeds and the fish dies.
+      * Note: "when a dog meets a fish" does not have to be the same as "when a
+        fish meets a dog" - but extra kudos to you if you can make it so.
     """
 
     # The classes Pet and Fish are taken from the notebook. copy and paste (and
     # modify) them into the other exercises as you need them.
     class Pet:
-        population = set()
-        
-        def __init__(self, name):
-            self.name = name
-            self.hunger = 0
-            self.age = 0
-            self.__class__.population.add(self)
-            
-        def die(self):
-            print("{} dies :(".format(self.name))
-            self.__class__.population.remove(self)
-            
-        def is_alive(self):
-            return self in self.__class__.population
-            
-        @classmethod
-        def advance_time(cls):
-            for individual in cls.population:
-                individual.age += 1
-                individual.hunger += 1
-            
-        def feed(self):
-            self.hunger = 0
-            
-        def hungrier_than(self, other_pet):
-            return self.hunger > other_pet.hunger
+	population = set()
+	
+	def __init__(self, name):
+	    self.name = name
+	    self.hunger = 0
+	    self.age = 0
+	    self.pets_met = set()
+	    self.__class__.population.add(self)
+	    
+	def die(self):
+	    print("{} dies :(".format(self.name))
+	    self.__class__.population.remove(self)
+	    
+	def is_alive(self):
+	    return self in self.__class__.population
+	    
+	@classmethod
+	def advance_time(cls):
+	    for pet in cls.population:
+		pet.age += 1
+		pet.hunger += 1
+	    
+	def feed(self):
+	    self.hunger = 0
+	    
+	def meet(self, other_pet):
+	    print("{} meets {}".format(self.name, other_pet.name))
+	    self.pets_met.add(other_pet)
+	    other_pet.pets_met.add(self)
+	    
+	def print_stats(self):
+	    print("{o.name}, age {o.age}, hunger {o.hunger}, met {n} others".
+		 format(o = self, n = len(self.pets_met)))
 
 
     class Fish(Pet):
-        def __init__(self, name, color):
-            self.color = color
-            super().__init__(name)
-            
-        def meet(self, other_fish):
-            if not isinstance(other_fish, Fish):
-                return
-            if self.color != other_fish.color:
-                if self.hungrier_than(other_fish):
-                    self.feed()
-                    other_fish.die()
-                elif other_fish.hungrier_than(self):
-                    other_fish.feed()
-                    self.die()
+	def __init__(self, name, size):
+	    self.size = size
+	    super().__init__(name)
+	    
+	def meet(self, other_fish):
+	    super().meet(other_fish)
+	    if not isinstance(other_fish, Fish):
+		return
+	    if self.size > other_fish.size:
+		self.feed()
+		other_fish.die()
+	    elif self.size < other_fish.size:
+		other_fish.feed()
+		self.die()
 
 
-    Dog = None # define dog here
+    Dog = None # make Dog class here
 
     return Pet, Fish, Dog
 
@@ -65,32 +72,38 @@ def make_dog_class():
 def test_dog_class():
     Pet, Fish, Dog = make_dog_class()
     assert type(Dog) == type
-    d = Dog("Attila")
-    assert hasattr(d, "happiness")
-    assert d.happiness == 100
-    d2 = Dog("Tamerlan")
+
+    attila = Dog("Attila")
+    assert hasattr(attila, "happiness")
+    assert attila.happiness == 100
+
+    tamerlan = Dog("Tamerlan")
+
     Pet.advance_time()
-    assert d.happiness == d2.happiness == 99
-    d.meet(d2)
-    assert d.happiness == d2.happiness == 100
-    f = Fish("Steve")
-    f.hunger = 10
-    assert d.hunger < f.hunger
-    assert d.hungrier_than(f)
-    assert d.hunger > 0
-    d.meet(f)
-    assert d.hunger == 0
-    assert not f.is_alive()
+    assert attila.happiness == tamerlan.happiness == 99
+
+    attila.meet(tamerlan)
+    assert attila.happiness == tamerlan.happiness == 100
+    assert attila in tamerlan.pets_met
+    assert tamerlan in attila.pets_met
+
+    steve = Fish("Steve")
+
+    assert attila.hunger > 0
+    attila.meet(steve)
+    assert attila.hunger == 0
+    assert not steve.is_alive()
 
 
 ###############################################################################
 
 def define_hungry():
     """
-    Copy the classes from the first exercise, and make the following happen:
+    Copy your classes from the first exercise, and make the following happen:
     * all pets have an `is_hungry()` method which returns True if the animal is
-      hungry, and False if not. in general, pets are considered to be hungry
-      when their hunger is > 50.
+      hungry, and False if not. In general, pets are considered to be hungry
+      when their hunger is > 50. Dogs, however, are considered to be hungry
+      when their hunger is > 10.
     * there is a classmethod `Pet.get_hungry_pets()` which returns the set of
       pets that are currently hungry.
     """
@@ -114,6 +127,3 @@ def test_define_hungry():
         assert x.is_hungry()
         assert Pet.get_hungry_pets() == {x}
         x.hunger = 0
-
-
-# TODO make it so that fish and dog have their own population.
